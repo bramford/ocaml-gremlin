11f62c9267d5333362c5bfe8185bb420180115dc
Bramford Horton <bram.horton@gmail.com>
2019-07-03 17:13:55 +1200

    Define result and check types

--- src/gremlin.ml
+++ src/gremlin.ml
@@ -5,4 +5,30 @@ module Websocket = struct
     let%lwt recv, send = Websocket_lwt_unix.with_connection ~ctx:Conduit_lwt_unix.default_ctx client uri in
     Lwt.return (recv, send)
 
+  type op_processor =
+    | Session of string
+    | Standard
+
+  type message_status =
+    | Response_invalid of response_invalid
+    | Request_failed of request_failed
+    | Unmatched_request_id
+    | Good of Yojson.Basic.t
+
+  and request_failed =
+    | Non_200_status_code of Yojson.Basic.t
+
+  and response_invalid =
+    | No_status_field
+    | Missing_fields
+    | Missing_status_fields
+    | Missing_field of string
+    | Invalid_status_json_type of Yojson.Basic.t
+    | Invalid_result_json_type of Yojson.Basic.t
+    | Invalid_result_field
+    | Invalid_request_id_json_type of Yojson.Basic.t
+    | Result_fields_wrong
+    | Json_parse_failure
+    | Unknown_json_parse_failure
+
 end
--- src/gremlin.mli
+++ src/gremlin.mli
@@ -3,4 +3,23 @@ module Websocket : sig
     Uri.t ->
     ((unit -> Websocket.Frame.t Lwt.t) * (Websocket.Frame.t -> unit Lwt.t))
     Lwt.t
+  type op_processor = Session of string | Standard
+  type message_status =
+      Response_invalid of response_invalid
+    | Request_failed of request_failed
+    | Unmatched_request_id
+    | Good of Yojson.Basic.t
+  and request_failed = Non_200_status_code of Yojson.Basic.t
+  and response_invalid =
+      No_status_field
+    | Missing_fields
+    | Missing_status_fields
+    | Missing_field of string
+    | Invalid_status_json_type of Yojson.Basic.t
+    | Invalid_result_json_type of Yojson.Basic.t
+    | Invalid_result_field
+    | Invalid_request_id_json_type of Yojson.Basic.t
+    | Result_fields_wrong
+    | Json_parse_failure
+    | Unknown_json_parse_failure
 end
