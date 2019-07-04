5b78a15ceb8f85bac8493166fd62ebfc42f88366
Bramford Horton <bram.horton@gmail.com>
2019-07-04 21:49:50 +1200

    Add response checker functions

--- src/gremlin.ml
+++ src/gremlin.ml
@@ -31,4 +31,74 @@ module Websocket = struct
     | Json_parse_failure
     | Unknown_json_parse_failure
 
+  let check_result = function
+    | Good m as message ->
+      begin try
+        let result = Yojson.Basic.Util.member "result" m in
+        if (CCList.equal
+             (String.equal)
+             (CCList.sort (String.compare) (Yojson.Basic.Util.keys result))
+             (CCList.sort (String.compare) ["data";"meta"])
+        ) then
+          message
+        else (
+          List.iter (Printf.printf "Result key: %s\n%!") (Yojson.Basic.Util.keys result);
+          Response_invalid Result_fields_wrong
+        )
+      with
+        | _ -> Response_invalid (Missing_field "result")
+        end
+    | _ as status -> status
+
+  let check_fields = function
+    | Good m as message ->
+      if (CCList.equal
+           (String.equal)
+           (Yojson.Basic.Util.keys m)
+           ["requestId";"status";"result"]
+      ) then
+        message
+      else (
+        List.iter (Printf.printf "Field key: %s\n%!") (Yojson.Basic.Util.keys m);
+        Response_invalid Missing_fields
+      )
+    | _ as status -> status
+
+  let check_status = function
+    | Good m as message ->
+      begin match Yojson.Basic.Util.member "status" m with
+        | `Null -> Response_invalid (Missing_field "status")
+        | _ as status ->
+          if (CCList.equal
+               (String.equal)
+               (CCList.sort (String.compare) (Yojson.Basic.Util.keys status))
+               (CCList.sort (String.compare) ["code";"attributes";"message"])
+          ) then (
+            begin match
+                Yojson.Basic.Util.member "code" status
+            with
+            | `Int i ->
+              begin match i with
+                | 200 -> message
+                | _ -> Request_failed (Non_200_status_code (Yojson.Basic.Util.member "status" m))
+              end
+            | json -> Response_invalid (Invalid_status_json_type json)
+            end
+          ) else (
+            Response_invalid Missing_status_fields
+          )
+      end
+    | _ as status -> status
+
+  let check_request_id request_id = function
+    | Good m as message ->
+      begin match Yojson.Basic.Util.member "requestId" m with
+        | `String s ->
+          if String.equal s request_id then
+            message
+          else
+            Unmatched_request_id
+      | json -> Response_invalid (Invalid_request_id_json_type json)
+      end
+    | _ as status -> status
 end
